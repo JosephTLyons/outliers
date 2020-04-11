1f690829a35d0c3da5fd5f3b5cfce93ac8ab7c96
Joseph T Lyons <JosephTLyons@gmail.com>
2020-04-11 02:20:31 -0400

    Remove get_average() - not part of this crate

--- src/lib.rs
+++ src/lib.rs
@@ -254,27 +254,3 @@ fn get_median_random_numbers_even_set() {
 fn get_median_random_numbers_odd_set() {
     assert!((get_median(&[1, 23, 24, 45, 200, 343, 1001]).unwrap() - 45.0).abs() < 0.0001);
 }
-
-pub fn get_average(data_vec: &[usize]) -> f32 {
-    if data_vec.is_empty() {
-        return 0.0;
-    }
-
-    let sum_of_file_name_lengths: usize = data_vec.iter().sum();
-    sum_of_file_name_lengths as f32 / data_vec.len() as f32
-}
-
-#[test]
-fn get_average_no_elements() {
-    assert!((get_average(&[])).abs() < 0.0001);
-}
-
-#[test]
-fn get_average_1() {
-    assert!((get_average(&[11, 100, 21, 34]) - 41.5).abs() < 0.0001);
-}
-
-#[test]
-fn get_average_2() {
-    assert!((get_average(&[3, 0, 49, 2000]) - 513.0).abs() < 0.0001);
-}
