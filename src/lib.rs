bf5030b32ad15a857b9908a2a839970878d23293
Joseph T Lyons <JosephTLyons@gmail.com>
2020-04-11 16:15:36 -0400

    Fixed doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use num::ToPrimitive;
 /// This function uses the Tukey method, which uses a multiplier value of 1.5. In the case that is
 /// does not return an `Err`, it returns a tuple of `Vec<T>`.  The first vector contains any lower
 /// outliers and the third vector contains any upper outliers.  Additionally, the second vector
-/// returned contains all the non-outliers; so that the data set passed in is returned in its
+/// returned contains all the non-outliers, so that the data set passed in is returned in its
 /// entirety as partitioned subsets.
 /// ```
 /// let data = [10, 12, 11, 15, 11, 14, 13, 17, 12, 22, 14, 11].to_vec();
